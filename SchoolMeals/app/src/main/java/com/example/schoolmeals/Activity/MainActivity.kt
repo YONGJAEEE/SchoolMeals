f29cb07bb9b0961e362dfdb2806e5726b08e847f
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-10-03 22:36:12 +0900

    [UPDATE] 날짜 변경 기능 구현

--- SchoolMeals/app/src/main/java/com/example/schoolmeals/Activity/MainActivity.kt
+++ SchoolMeals/app/src/main/java/com/example/schoolmeals/Activity/MainActivity.kt
@@ -14,8 +14,11 @@ import com.example.schoolmeals.R
 import com.example.schoolmeals.Responses.MealsResponse
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.breakfastpage.*
+import kotlinx.android.synthetic.main.breakfastpage.view.*
 import kotlinx.android.synthetic.main.dinnerpage.*
+import kotlinx.android.synthetic.main.dinnerpage.view.*
 import kotlinx.android.synthetic.main.lunchpage.*
+import kotlinx.android.synthetic.main.lunchpage.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,15 +33,17 @@ class MainActivity : AppCompatActivity() {
 
     val BASE_URL = "https://kyj-school-server.herokuapp.com/"
 
-    var dateToday = (Timestamp(System.currentTimeMillis()).time / 1000L) * 1000 + 518400000
+    var dateToday = (Timestamp(System.currentTimeMillis()).time / 1000L) * 1000 + 86400000 + 86400000
+
+    var ScName = MyApplication.prefs.getString("SchoolName", "null")
+    var ascCode = MyApplication.prefs.getString("ascCode", "null")
+    var scCode = MyApplication.prefs.getString("scCode", "null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var ScName = MyApplication.prefs.getString("SchoolName", "null")
-        var ascCode = MyApplication.prefs.getString("ascCode", "null")
-        var scCode = MyApplication.prefs.getString("scCode", "null")
+
 
         viewList.add(layoutInflater.inflate(R.layout.breakfastpage, null))
         viewList.add(layoutInflater.inflate(R.layout.lunchpage, null))
@@ -56,10 +61,19 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("time", dateToday.toString())
 
-        getMeals()
+        getMeals(dateToday)
+
+        btn_before.setOnClickListener(){
+            dateToday -= 86400000
+            getMeals(dateToday)
+        }
+        btn_next.setOnClickListener(){
+            dateToday += 86400000
+            getMeals(dateToday)
+        }
     }
 
-    fun getMeals() {
+    fun getMeals(date : Long) {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -67,7 +81,7 @@ class MainActivity : AppCompatActivity() {
 
         val service = retrofit.create(MealsAPI::class.java)
 
-        val call = service.CallMeals("D10", "7240393", dateToday.toString())
+        val call = service.CallMeals(ascCode, scCode, date.toString())
 
         call.enqueue(object : Callback<MealsResponse> {
             override fun onResponse(call: Call<MealsResponse>, response: Response<MealsResponse>) {
@@ -76,13 +90,13 @@ class MainActivity : AppCompatActivity() {
                 tv_date.text = response.body()?.time.toString()
 
                 if (response.body()?.breakfast?.lists != null) {
-                    tv_breakfast.setText(response.body()?.breakfast?.lists.toString().replace("<br/>","\n"))
+                    viewList[0].tv_breakfast.setText(response.body()?.breakfast?.lists.toString().replace("<br/>","\n"))
                 }
                 if (response.body()?.lunch?.lists != null) {
-                    tv_lunch.setText(response.body()?.lunch?.lists.toString().replace("<br/>","\n"))
+                    viewList[1].tv_lunch.setText(response.body()?.lunch?.lists.toString().replace("<br/>","\n"))
                 }
                 if (response.body()?.dinner?.lists != null) {
-                    tv_dinner.setText(response.body()?.dinner?.lists.toString().replace("<br/>","\n"))
+                    viewList[2].tv_dinner.setText(response.body()?.dinner?.lists.toString().replace("<br/>","\n"))
                 }
             }
 
